58f29f043e01aa9f4599839a8bc496d96036db72
cr0barsCorner <cr0barsCorner@proton.me>
2023-12-29 00:01:47 -0500

    Added new post

--- scripts/visitor-counter.js
+++ scripts/visitor-counter.js
@@ -10,8 +10,7 @@ const value_sprites = [
 	"counter-7.png",
 	"counter-8.png",
 	"counter-9.png"
-	];
-
+];
 const counter_ids = [
 	"counter-one",
 	"counter-ten",
@@ -20,13 +19,15 @@ const counter_ids = [
 	"counter-ten-thousand",
 	"counter-hundred-thousand",
 	"counter-million"
-	];
+];
+const API_target = "http://localhost:3002/api?";
 
-var total_visitors = 69420;
+var total_visitors = 0;
+var total_pageviews = 0;
 
-function updateCounter()
+function updateCatgirlCounter(value)
 {
-	var arrayOfDigits = Array.from(String(total_visitors), Number);
+	var arrayOfDigits = Array.from(String(value), Number);
 	arrayOfDigits.reverse();
 	
 	for (let i = 0; i < counter_ids.length; i++)
@@ -36,7 +37,23 @@ function updateCounter()
 		document.getElementById(counter_ids[counter_ids.length - 1 - i]).src = sprite_path + value_sprites[0];
 }
 
-function requrestVisitorCount()
-{}
+async function updateServerCounter(type)
+{
+	const response = await fetch(API_target + type);
+	const data = await response.json();
+	total_visitors = Number(data.visits);
+	total_pageviews = Number(data.views);
+	updateCatgirlCounter(total_visitors);
+}
 
-updateCounter()
+function mainActionOnPageLoad()
+{
+	if (sessionStorage.getItem('visit') === null) // new visit and page view
+		updateServerCounter('type=visit-view');
+	else // just page view
+		updateServerCounter('type=view');
+		
+	sessionStorage.setItem('visit', 'true');
+}
+
+mainActionOnPageLoad();
